93f13b1170573bd64fd24ba10113c3f959aa2cab
Jesfer Babao <jesferb@codev.com>
2024-11-11 19:33:40 +0800

    Add default port

--- ProjectName.API/src/config/config.ts
+++ ProjectName.API/src/config/config.ts
@@ -33,7 +33,7 @@ export const MONGO_OPTIONS: mongoose.ConnectOptions = {
 
 // Server configuration
 export const SERVER_HOSTNAME = process.env.SERVER_HOST || "localhost";
-export const SERVER_PORT = process.env.SERVER_PORT
+export const SERVER_PORT = process.env.SERVER_PORT || 3000;
   ? Number(process.env.SERVER_PORT)
   : 3000;
 
